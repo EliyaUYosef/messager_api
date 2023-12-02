8ade4ee2eff9de2a7729dc3cbd7a1cc1ba0c714b
Eliya.Y <eliya.yosef@gmail.com>
2023-12-03 00:41:11 +0200

    Auto deployment: 2023-12-03 00:41:11

--- on_load.sh
+++ on_load.sh
@@ -8,13 +8,13 @@ heroku run php artisan cache:clear
 heroku run php artisan route:clear
 
 # Drop all MySQL tables
-# heroku run php artisan migrate:reset
+heroku run php artisan migrate:reset
 
 # Git operations
 git add .
 git commit -m "Auto deployment: $(date +'%Y-%m-%d %H:%M:%S')"
-git push
 git push heroku main
+git push
 
 # Additional Laravel optimizations
 heroku run php artisan clear-compiled
@@ -24,9 +24,9 @@ heroku run php artisan optimize
 heroku run chmod -R 777 storage
 
 # Migrate database
-# heroku run php artisan migrate
+heroku run php artisan migrate
 
 # Passport keys and client
-# heroku run php artisan passport:keys --force
-heroku run php artisan passport:keys
+heroku run php artisan passport:keys --force
+# heroku run php artisan passport:keys
 heroku run php artisan passport:client --personal
